724e44847fbd4ec3698788ff0f01060ce32fdbc4
iotataru <63681485+iotataru@users.noreply.github.com>
2021-02-04 17:35:09 -0800

    Reattempt separate js from html

---
+++ DialogApi/ChildPage.js
@@ -0,0 +1,45 @@
+"use strict";
+// The initialize function must be run each time a new page is loaded
+window.Office.initialize = function (reason) {
+    debugger;
+    console.log("window.Office.initialize called");
+    RegisterMessageChild();
+}
+
+//*********************message parent
+function messageParent() {
+  var value = document.getElementById("MessageForParent").value;
+    if (!value) {
+        value = "Message For Parent";
+    }
+
+    Office.context.ui.messageParent(value);
+}
+
+function showNotification(text) {
+    document.getElementById('actionResult').innerText += text;
+}
+
+function addMessageStatus(arg) {
+    showNotification(arg.message);
+}
+
+function RegisterMessageChild() {
+  Office.context.ui.addHandlerAsync(Office.EventType.DialogParentMessageReceived, addMessageStatus, onRegisterMessageComplete);
+}
+
+function onRegisterMessageComplete(asyncResult) {
+  document.getElementById('actionResult').innerText += asyncResult.status;
+  if(asyncResult.status != Office.AsyncResultStatus.Succeeded) {
+    document.getElementById('actionResult').innerText += asyncResult.error.message;
+  }
+}
+
+function redirect() {
+    var value = document.getElementById("RedirectWebsite").value;
+    if (!value) {
+        console.log("Error: need a website in the textbox.");
+        return;
+    }
+    window.location.href = value;
+}
---
+++ DialogApi/ParentPage.js
@@ -0,0 +1,91 @@
+"use strict";
+// The initialize function must be run each time a new page is loaded
+var _dialog;
+var _childPageUrl = "https://iotataru.github.io/DialogApi/ChildPage.html";
+
+window.onload = function() {
+  document.getElementById('curentSource').innerHTML = "SOURCE: " + document.querySelector('[title="Office Add-in TwoWayMessageDialogTest"]').src;
+}
+
+function showNotification(text) {
+    document.getElementById('actionResult').innerText += text;
+}
+
+function launchDialogCallback(arg) {
+    if (arg.status === "failed") {
+        showNotification("launch dialog failed");
+    }
+    else {
+        _dialog = arg.value;
+        _dialog.addEventHandler(Microsoft.Office.WebExtension.EventType.DialogMessageReceived, addMessageStatus);
+        _dialog.addEventHandler(Microsoft.Office.WebExtension.EventType.DialogEventReceived, addCloseStatus);
+  setTimeout(messageChildInitial, 2000);
+    }
+}
+
+function addMessageStatus(arg) {
+    showNotification(arg.message);
+}
+
+function addCloseStatus(arg) {
+    showNotification("dialog closed");
+}
+
+function launchInlineDialog() {
+    var dialogUrl = !!(document.getElementById("InlineLaunch").value) ? document.getElementById("InlineLaunch").value : _childPageUrl;
+    Office.context.ui.displayDialogAsync(dialogUrl,
+  {height:80, width:50, hideTitle: false, promptBeforeOpen:true, enforceAppDomain: true, displayInIframe:true},
+  launchDialogCallback);
+}
+
+function launchWindowDialog() {
+    var dialogUrl = !!(document.getElementById("WindowLaunch").value) ? document.getElementById("WindowLaunch").value : _childPageUrl;
+    Office.context.ui.displayDialogAsync(dialogUrl,
+  {height:80, width:50, hideTitle: false, promptBeforeOpen:true, enforceAppDomain: true},
+  launchDialogCallback);
+}
+
+function launchInlineDialogFromRibbon(args) {
+    Office.context.ui.displayDialogAsync(_childPageUrl, { height: 50, width: 30, displayInIframe: true }, launchDialogCallback);
+
+    args.completed();
+}
+
+function launchWindowDialogFromRibbon(args) {
+    Office.context.ui.displayDialogAsync(_childPageUrl, { height: 50, width: 30, displayInIframe: false }, launchDialogCallback);
+
+    args.completed();
+}
+
+function messageChildInitial() {
+    messageChild("Initial message for child upon parent's launchDialogCallback");
+}
+
+function messageChild() {
+    messageChild("");
+}
+
+function messageChild(message) {
+    var value = document.getElementById("MessageForChild").value;
+    if (!value) {
+  value = message;
+  if (!value) {
+      value = "Message For Child";
+  }
+    }
+
+    _dialog.messageChild(value);
+}
+
+function closeDialog() {
+    _dialog.close();
+}
+
+function redirect() {
+    var value = document.getElementById("RedirectWebsite").value;
+    if (!value) {
+        console.log("Error: need a website in the textbox.");
+        return;
+    }
+    window.location.href = value;
+}
